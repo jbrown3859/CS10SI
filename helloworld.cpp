1ffc7dcf5267869f44d95ea2bf700a2b8b6bd476
Joseph Brown <jbrown3859@gmail.com>
2019-10-03 10:48:08 -0700

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+// Example program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string name,y;
+  getline(cin,name);
+  cin.clear();
+  getline(cin,y);
+  cout << y+name;
+
+  return 0;
+}
