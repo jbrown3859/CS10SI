b52cb531cf12b5d9214af5dd1d770fa354b95851
Joseph Brown <jbrown3859@gmail.com>
2019-11-14 10:49:32 -0800

    update

---
+++ conversion_dist.cpp
@@ -0,0 +1,19 @@
+int convertkm2m(double km) {
+  return km*1000;
+}
+int convertm2cm(int m) {
+  return m*100;
+}
+int convertcm2mm(int cm) {
+  return cm*10;
+}
+
+
+
+
+
+
+int main() {
+
+  return 0;
+}
